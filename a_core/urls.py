23bc4fd937004e279609374cb5a104572ceb3c1c
EslamKamel89 <elsamkamelforex@gmail.com>
2026-01-18 11:30:13 +0200

    feat(core): add django-extensions and wire allauth account routes

--- a_core/urls.py
+++ a_core/urls.py
@@ -24,4 +24,5 @@ urlpatterns = [
     path("admin/", admin.site.urls),
     path("", include("a_home.urls")),
     path("auth/", include("a_users.urls")),
+    path("accounts/", include("allauth.urls")),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
