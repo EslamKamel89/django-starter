6e1081369d0a3fc4ccf9ee785a26dcfe1931530f
EslamKamel89 <elsamkamelforex@gmail.com>
2026-01-18 08:07:26 +0200

    feat(users): fix avatar binding, enable crispy forms, and add profile edit route

--- a_users/urls.py
+++ a_users/urls.py
@@ -3,5 +3,6 @@ from django.urls import URLPattern, path
 from . import views
 
 urlpatterns: list[URLPattern] = [
-    path("profile", views.ProfileView.as_view(), name="my-profile")
+    path("profile", views.ProfileView.as_view(), name="my-profile"),
+    path("profile/edit", views.ProfileEditView.as_view(), name="my-profile-edit"),
 ]
