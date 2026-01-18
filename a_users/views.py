ab1e84505239a7c041b0059f575a942acdb83463
EslamKamel89 <elsamkamelforex@gmail.com>
2026-01-18 10:46:16 +0200

    feat(users): handle profile edit submission with validation and flash messages

--- a_users/views.py
+++ a_users/views.py
@@ -1,6 +1,8 @@
+from django.contrib import messages
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.http import HttpRequest
-from django.shortcuts import render
+from django.shortcuts import redirect, render
+from django.urls import reverse
 from django.views import View
 
 from a_users.forms import ProfileForm
@@ -16,4 +18,14 @@ class ProfileEditView(LoginRequiredMixin, View):
     def get(self, request: HttpRequest):
         profile = request.user.profile  # type: ignore
         form = ProfileForm(instance=profile)
+        return render(request, "a_users/profile-edit.html", context={"form": form})
+
+    def post(self, request: HttpRequest):
+        profile = request.user.profile  # type: ignore
+        form = ProfileForm(instance=profile, data=request.POST, files=request.FILES)
+        if form.is_valid():
+            profile = form.save()
+            messages.success(request, "Your profile updated successfully")
+            return redirect(reverse("my-profile"))
+        messages.error(request, "Please fix the validation error")
         return render(request, "a_users/profile-edit.html", context={"form": form})
